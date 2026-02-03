0bb115a062606ddf7ebe4571fa6bec615d63401d
João <joaoalvesxp@alu.ufc.br>
2026-02-03 03:14:25 -0300

    feat: usando tema do primevue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,22 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import "./style.css";
 
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+
+app.use(router);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: ".p-dark",
+        },
+    },
+});
+
+app.mount("#app");
